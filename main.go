1dd51a2a0fa6d27006b7008a82e8d3d2922eeb8f
Yasser Badr <67002064+Yasser-Badr@users.noreply.github.com>
2026-09-18 03:58:02 +0300

    Update DSN string in ConnectDB function
    Removed 'TimeZone=Asia/Kuwait' from the DSN string in ConnectDB function.

--- main.go
+++ main.go
@@ -73,13 +73,13 @@ type AdminUser struct {
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kuwait",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	os.Getenv("DB_HOST"),
+	os.Getenv("DB_USER"),
+	os.Getenv("DB_PASSWORD"),
+	os.Getenv("DB_NAME"),
+	os.Getenv("DB_PORT"),
+)
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
